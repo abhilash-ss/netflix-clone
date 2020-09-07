c8d869686d65ff05962707c6d6d420bbcdeebb73
itsmeAB <ss.abhilash3@gmail.com>
2020-09-08 02:24:46 +0530

    Add global types file

--- src/requests.ts
+++ src/requests.ts
@@ -1,6 +1,8 @@
-const API_KEY = "7836ba7d33df226623eb6055ffd35b5e";
+import { requestsType } from "./types";
 
-const requests: Object = {
+const API_KEY: string = "7836ba7d33df226623eb6055ffd35b5e";
+
+const requests: requestsType = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type requestsType = {
+  fetchTrending: string;
+  fetchNetflixOriginals: string;
+  fetchTopRated: string;
+  fetchActionMovies: string;
+  fetchComedyMovies: string;
+  fetchHorrorMovies: string;
+  fetchRomanceMovies: string;
+  fetchDocumentaries: string;
+};
